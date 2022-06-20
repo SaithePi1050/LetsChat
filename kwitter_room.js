0f04f2288f66ebba3303f93686cd77e748904b54
SaithePi <89380734+SaithePi1050@users.noreply.github.com>
2022-06-20 19:52:22 +1000

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -5,12 +5,31 @@ document.getElementById("user_name").innerHTML = "Welcome " + user_name;
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      console.log("room_name" + Room_names);
+      row = "<div class = 'room_name' id = " + Room_names + " onclick = 'back_to_room_page(this.id)'>#" +  Room_names + "</div><hr>";
       //End code
+      document.getElementById("output").innerHTML += row;
       });});}
 getData();
+
 function add_room() 
 {
       var room_name = document.getElementById("room_name").value;
       firebase.database().ref("/").child(room_name).update({Purpose : "1", Purpose1 : "2"});
+      localStorage.setItem("room_name", room_name);
+      window.location = "LetsChat_message_page.html";
+}
+
+function back_to_room_page(name) 
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "LetsChat_message_page.html";
+}
+
+function logout() 
+{
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("roon_name");
+      window.location = "index.html";
 }
